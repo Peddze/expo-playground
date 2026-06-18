081cf9e5cbb5d337f1a0d95ccf275552b0220ed8
Pedro Reis <peddze@proton.me>
2026-06-18 16:28:48 +0100

    feat: add core RN components with pressable handler

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,6 +10,11 @@ export default function Index() {
       }}
     >
       <Text>This is the start of my finance app</Text>
+      <Pressable onPress={() => {
+          console.log("Pressed");
+        }}>
+        <Text>Click me</Text>
+      </Pressable>
     </View>
   );
 }
